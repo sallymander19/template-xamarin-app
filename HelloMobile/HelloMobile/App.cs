f51c9a25e3ebb3cf58ae5c34fd0edaafa8865546
sallymander19 <sallymander19@users.noreply.github.com>
2017-02-06 19:23:20 -0600

    changes

--- HelloMobile/HelloMobile/App.cs
+++ HelloMobile/HelloMobile/App.cs
@@ -21,7 +21,7 @@ namespace HelloMobile
                     Children = {
                         new Label {
                             HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
+                            Text = "Hello .Net Mobile Class"
                         }
                     }
                 }
